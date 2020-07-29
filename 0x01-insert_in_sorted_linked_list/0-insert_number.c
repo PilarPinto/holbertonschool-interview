7bf4f2ee743841b1d360038caf0703a7f9d29aba
PilarPinto <piapintoch@unal.edu.co>
2020-07-29 00:56:55 +0000

    [Test]:Adding condition

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -19,9 +19,9 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (*head == NULL || (*head)->n >= new_node->n)
+	if (*head == NULL || (*head)->n > new_node->n)
 	{
-		new_node->next= NULL;
+		new_node->next = *head;
 		*head = new_node;
 	}
 	else
