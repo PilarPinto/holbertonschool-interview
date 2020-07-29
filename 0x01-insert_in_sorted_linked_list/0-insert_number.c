fcc19073daf8ab0b489894ccd28647fefa02ffb2
PilarPinto <piapintoch@unal.edu.co>
2020-07-29 00:38:54 +0000

    [Feature]:Insert linked list function

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - Insert a node in a SLL
+ * @head: Is the pointer to direction of a list
+ * @number: Is the data value
+ * Return:The new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		while (current->next != NULL && current->next->n < new_node->n)
+			current = current->next;
+		new_node->next = current->next;
+		current->next = new_node;
+	}
+	return (new_node);
+}
