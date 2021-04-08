c9944ba8832a78f021b6d8c25a5ccb617e8ef710
PilarPinto <piapintoch@unal.edu.co>
2021-04-08 00:05:43 +0000

    [T]

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,36 @@
 #!/usr/bin/node
-/* Star Wars API info */
 const request = require('request');
-const urlAcc = 'https://swapi-api.hbtn.io/api/films/';
-const movPos = process.argv[2];
-// query
-request(urlAcc + moviPos, (error, response, body) => {
-  if (error) throw error;
-  const characters = JSON.parse(body).characters;
-  showNames(characters);
-});
-// console print
-const showNames = (names, i = 0) => {
-  if (i === names.length) return;
-  request(names[i], (error, response, body) => {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    showNames(names, i + 1);
-  });
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
+  headers: {
+    'User-Agent': 'request'
+  }
 };
+
+getData();
+
+function getData () {
+  request(options, async (err, response, body) => {
+    if (!err) {
+      for (const people of JSON.parse(body).characters) {
+        await getCharactersNames(people);
+      }
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function getCharactersNames (people) {
+  return new Promise((resolve, reject) => {
+    request(people, (err, response, body) => {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
